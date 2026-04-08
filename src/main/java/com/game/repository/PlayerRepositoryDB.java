d805c94e518d669a93d43dedc4f1b836a3a532bc
Raritetnik <mkodarenko@gmail.com>
2026-04-07 21:04:12 -0400

    Исправляем проэкт для запуска под современную версию и берём данные из базы данных.

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,22 +1,63 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    public PlayerRepositoryDB() {
+    private final SessionFactory sessionFactory;
 
+    public PlayerRepositoryDB() {
+        Properties properties = new Properties();
+
+        // DB connection
+        properties.put(Environment.DRIVER, "com.p6spy.engine.spy.P6SpyDriver");
+        properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg");
+        properties.put(Environment.USER, "root");
+        properties.put(Environment.PASS, "root");
+
+        // Hibernate settings
+        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+        properties.put(Environment.SHOW_SQL, "true");
+
+        // Авто создание/обновление таблиц
+        properties.put(Environment.HBM2DDL_AUTO, "update");
+
+        // Optional
+        properties.put(Environment.FORMAT_SQL, "true");
+
+        // Configuration
+        Configuration configuration = new Configuration();
+        configuration.setProperties(properties);
+
+        // Добавляем Entity
+        configuration.addAnnotatedClass(Player.class);
+
+        // Создаем SessionFactory
+        sessionFactory = configuration.buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        try (Session session = sessionFactory.openSession()) {
+            NativeQuery<Player> query = session.createNativeQuery(
+                    "SELECT * FROM player", Player.class
+            );
+            query.setFirstResult(pageNumber * pageSize);
+            query.setMaxResults(pageSize);
+            return query.getResultList();
+        }
     }
 
     @Override
